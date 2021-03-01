f60aa58fdc8779c1ed59a7dbfa0b8628cb204cce
Prashant1503 <44309149+Prashant1503@users.noreply.github.com>
2021-03-01 11:28:48 +0530

    Solved unstructure data

---
+++ problemOne.js
@@ -0,0 +1,694 @@
+
+// let data = {
+//   "resources" : [
+//     {
+//       "id": 2166,
+//       "roleId": 4,
+//       "permissionId": 1,
+//       "createdBy": 41,
+//       "updatedBy": 41,
+//       "createdAt": "2020-10-31T07:08:43.000Z",
+//       "updatedAt": "2020-10-31T07:08:43.000Z",
+//       "permission": {
+//         "id": 1,
+//         "action": "READ",
+//         "resource": "CAUSE"
+//       }
+//     },
+//     {
+//       "id": 2167,
+//       "roleId": 4,
+//       "permissionId": 2,
+//       "createdBy": 41,
+//       "updatedBy": 41,
+//       "createdAt": "2020-10-31T07:08:43.000Z",
+//       "updatedAt": "2020-10-31T07:08:43.000Z",
+//       "permission": {
+//         "id": 2,
+//         "action": "WRITE",
+//         "resource": "CAUSE"
+//       }
+//     },
+//     {
+//       "id": 2168,
+//       "roleId": 4,
+//       "permissionId": 3,
+//       "createdBy": 41,
+//       "updatedBy": 41,
+//       "createdAt": "2020-10-31T07:08:43.000Z",
+//       "updatedAt": "2020-10-31T07:08:43.000Z",
+//       "permission": {
+//         "id": 3,
+//         "action": "PUBLISH",
+//         "resource": "CAUSE"
+//       }
+//     },
+//     {
+//       "id": 2169,
+//       "roleId": 4,
+//       "permissionId": 13,
+//       "createdBy": 41,
+//       "updatedBy": 41,
+//       "createdAt": "2020-10-31T07:08:43.000Z",
+//       "updatedAt": "2020-10-31T07:08:43.000Z",
+//       "permission": {
+//         "id": 13,
+//         "action": "READ",
+//         "resource": "CONTACT"
+//       }
+//     },
+//     {
+//       "id": 2170,
+//       "roleId": 4,
+//       "permissionId": 14,
+//       "createdBy": 41,
+//       "updatedBy": 41,
+//       "createdAt": "2020-10-31T07:08:43.000Z",
+//       "updatedAt": "2020-10-31T07:08:43.000Z",
+//       "permission": {
+//         "id": 14,
+//         "action": "EDIT",
+//         "resource": "CONTACT"
+//       }
+//     },
+//     {
+//       "id": 2171,
+//       "roleId": 4,
+//       "permissionId": 15,
+//       "createdBy": 41,
+//       "updatedBy": 41,
+//       "createdAt": "2020-10-31T07:08:43.000Z",
+//       "updatedAt": "2020-10-31T07:08:43.000Z",
+//       "permission": {
+//         "id": 15,
+//         "action": "WRITE",
+//         "resource": "CONTACT"
+//       }
+//     },
+//     {
+//       "id": 2172,
+//       "roleId": 4,
+//       "permissionId": 16,
+//       "createdBy": 41,
+//       "updatedBy": 41,
+//       "createdAt": "2020-10-31T07:08:43.000Z",
+//       "updatedAt": "2020-10-31T07:08:43.000Z",
+//       "permission": {
+//         "id": 16,
+//         "action": "DELETE",
+//         "resource": "CONTACT"
+//       }
+//     },
+//     {
+//       "id": 2173,
+//       "roleId": 4,
+//       "permissionId": 8,
+//       "createdBy": 41,
+//       "updatedBy": 41,
+//       "createdAt": "2020-10-31T07:08:43.000Z",
+//       "updatedAt": "2020-10-31T07:08:43.000Z",
+//       "permission": {
+//         "id": 8,
+//         "action": "READ",
+//         "resource": "CAMPAIGN"
+//       }
+//     },
+//     {
+//       "id": 2174,
+//       "roleId": 4,
+//       "permissionId": 24,
+//       "createdBy": 41,
+//       "updatedBy": 41,
+//       "createdAt": "2020-10-31T07:08:43.000Z",
+//       "updatedAt": "2020-10-31T07:08:43.000Z",
+//       "permission": {
+//         "id": 24,
+//         "action": "WRITE",
+//         "resource": "CAMPAIGN"
+//       }
+//     },
+//     {
+//       "id": 2175,
+//       "roleId": 4,
+//       "permissionId": 25,
+//       "createdBy": 41,
+//       "updatedBy": 41,
+//       "createdAt": "2020-10-31T07:08:43.000Z",
+//       "updatedAt": "2020-10-31T07:08:43.000Z",
+//       "permission": {
+//         "id": 25,
+//         "action": "DELETE",
+//         "resource": "CAMPAIGN"
+//       }
+//     },
+//     {
+//       "id": 2176,
+//       "roleId": 4,
+//       "permissionId": 26,
+//       "createdBy": 41,
+//       "updatedBy": 41,
+//       "createdAt": "2020-10-31T07:08:43.000Z",
+//       "updatedAt": "2020-10-31T07:08:43.000Z",
+//       "permission": {
+//         "id": 26,
+//         "action": "EDIT",
+//         "resource": "CAMPAIGN"
+//       }
+//     },
+//     {
+//       "id": 2177,
+//       "roleId": 4,
+//       "permissionId": 21,
+//       "createdBy": 41,
+//       "updatedBy": 41,
+//       "createdAt": "2020-10-31T07:08:43.000Z",
+//       "updatedAt": "2020-10-31T07:08:43.000Z",
+//       "permission": {
+//         "id": 21,
+//         "action": "ARCHIVE",
+//         "resource": "EMAILTEMPLATE"
+//       }
+//     },
+//     {
+//       "id": 2178,
+//       "roleId": 4,
+//       "permissionId": 20,
+//       "createdBy": 41,
+//       "updatedBy": 41,
+//       "createdAt": "2020-10-31T07:08:43.000Z",
+//       "updatedAt": "2020-10-31T07:08:43.000Z",
+//       "permission": {
+//         "id": 20,
+//         "action": "PUBLISH",
+//         "resource": "EMAILTEMPLATE"
+//       }
+//     },
+//     {
+//       "id": 2179,
+//       "roleId": 4,
+//       "permissionId": 19,
+//       "createdBy": 41,
+//       "updatedBy": 41,
+//       "createdAt": "2020-10-31T07:08:43.000Z",
+//       "updatedAt": "2020-10-31T07:08:43.000Z",
+//       "permission": {
+//         "id": 19,
+//         "action": "DELETE",
+//         "resource": "EMAILTEMPLATE"
+//       }
+//     },
+//     {
+//       "id": 2180,
+//       "roleId": 4,
+//       "permissionId": 18,
+//       "createdBy": 41,
+//       "updatedBy": 41,
+//       "createdAt": "2020-10-31T07:08:43.000Z",
+//       "updatedAt": "2020-10-31T07:08:43.000Z",
+//       "permission": {
+//         "id": 18,
+//         "action": "EDIT",
+//         "resource": "EMAILTEMPLATE"
+//       }
+//     },
+//     {
+//       "id": 2181,
+//       "roleId": 4,
+//       "permissionId": 17,
+//       "createdBy": 41,
+//       "updatedBy": 41,
+//       "createdAt": "2020-10-31T07:08:43.000Z",
+//       "updatedAt": "2020-10-31T07:08:43.000Z",
+//       "permission": {
+//         "id": 17,
+//         "action": "WRITE",
+//         "resource": "EMAILTEMPLATE"
+//       }
+//     },
+//     {
+//       "id": 2182,
+//       "roleId": 4,
+//       "permissionId": 9,
+//       "createdBy": 41,
+//       "updatedBy": 41,
+//       "createdAt": "2020-10-31T07:08:43.000Z",
+//       "updatedAt": "2020-10-31T07:08:43.000Z",
+//       "permission": {
+//         "id": 9,
+//         "action": "READ",
+//         "resource": "EMAILTEMPLATE"
+//       }
+//     },
+//     {
+//       "id": 2183,
+//       "roleId": 4,
+//       "permissionId": 23,
+//       "createdBy": 41,
+//       "updatedBy": 41,
+//       "createdAt": "2020-10-31T07:08:43.000Z",
+//       "updatedAt": "2020-10-31T07:08:43.000Z",
+//       "permission": {
+//         "id": 23,
+//         "action": "DELETE",
+//         "resource": "SUPPRESSIONLIST"
+//       }
+//     },
+//     {
+//       "id": 2184,
+//       "roleId": 4,
+//       "permissionId": 22,
+//       "createdBy": 41,
+//       "updatedBy": 41,
+//       "createdAt": "2020-10-31T07:08:43.000Z",
+//       "updatedAt": "2020-10-31T07:08:43.000Z",
+//       "permission": {
+//         "id": 22,
+//         "action": "READ",
+//         "resource": "SUPPRESSIONLIST"
+//       }
+//     },
+//     {
+//       "id": 2185,
+//       "roleId": 4,
+//       "permissionId": 7,
+//       "createdBy": 41,
+//       "updatedBy": 41,
+//       "createdAt": "2020-10-31T07:08:43.000Z",
+//       "updatedAt": "2020-10-31T07:08:43.000Z",
+//       "permission": {
+//         "id": 7,
+//         "action": "READ",
+//         "resource": "IDLIST"
+//       }
+//     },
+//     {
+//       "id": 2186,
+//       "roleId": 4,
+//       "permissionId": 10,
+//       "createdBy": 41,
+//       "updatedBy": 41,
+//       "createdAt": "2020-10-31T07:08:43.000Z",
+//       "updatedAt": "2020-10-31T07:08:43.000Z",
+//       "permission": {
+//         "id": 10,
+//         "action": "EDIT",
+//         "resource": "IDLIST"
+//       }
+//     },
+//     {
+//       "id": 2187,
+//       "roleId": 4,
+//       "permissionId": 11,
+//       "createdBy": 41,
+//       "updatedBy": 41,
+//       "createdAt": "2020-10-31T07:08:43.000Z",
+//       "updatedAt": "2020-10-31T07:08:43.000Z",
+//       "permission": {
+//         "id": 11,
+//         "action": "WRITE",
+//         "resource": "IDLIST"
+//       }
+//     },
+//     {
+//       "id": 2188,
+//       "roleId": 4,
+//       "permissionId": 12,
+//       "createdBy": 41,
+//       "updatedBy": 41,
+//       "createdAt": "2020-10-31T07:08:43.000Z",
+//       "updatedAt": "2020-10-31T07:08:43.000Z",
+//       "permission": {
+//         "id": 12,
+//         "action": "DELETE",
+//         "resource": "IDLIST"
+//       }
+//     },
+//     {
+//       "id": 2189,
+//       "roleId": 4,
+//       "permissionId": 27,
+//       "createdBy": 41,
+//       "updatedBy": 41,
+//       "createdAt": "2020-10-31T07:08:43.000Z",
+//       "updatedAt": "2020-10-31T07:08:43.000Z",
+//       "permission": {
+//         "id": 27,
+//         "action": "EXPORT",
+//         "resource": "IDLIST"
+//       }
+//     },
+//     {
+//       "id": 2190,
+//       "roleId": 4,
+//       "permissionId": 5,
+//       "createdBy": 41,
+//       "updatedBy": 41,
+//       "createdAt": "2020-10-31T07:08:43.000Z",
+//       "updatedAt": "2020-10-31T07:08:43.000Z",
+//       "permission": {
+//         "id": 5,
+//         "action": "APPROVE",
+//         "resource": "CAUSE"
+//       }
+//     },
+//     {
+//       "id": 2191,
+//       "roleId": 4,
+//       "permissionId": 6,
+//       "createdBy": 41,
+//       "updatedBy": 41,
+//       "createdAt": "2020-10-31T07:08:43.000Z",
+//       "updatedAt": "2020-10-31T07:08:43.000Z",
+//       "permission": {
+//         "id": 6,
+//         "action": "FEATURE",
+//         "resource": "CAUSE"
+//       }
+//     }
+//   ]
+// }
+
+
+
+
+// function getData () {
+
+//   var type;
+//   var result;
+//   var pData;
+//   let permissionData;
+//   var causeArr = [];
+//   var campaignArr = [];
+//   var emailTemplate = [];
+//   var supressArr = [];
+//   var idList = [];
+//   var arr;
+
+//   var Obj = {};
+
+//   var newArr = [];
+
+
+  
+//     arr = data.resources;
+
+
+//     arr.map((value) => {
+      
+//       // getting permission key
+//       for(let childKey in value) {
+       
+//         pData = value[childKey];
+        
+//       }
+
+//       type = pData.resource; //resource : 
+
+  
+//       if(type == "CAUSE") {
+         
+//         causeArr.push(pData);
+//         Obj[type] = causeArr;
+//       }
+
+     
+//       else if(type == "CAMPAIGN") {
+       
+//         campaignArr.push(pData);
+//         Obj[type] = campaignArr;
+//       }
+
+//       else if(type == "EMAILTEMPLATE") {
+//         emailTemplate.push(pData);
+//         Obj[type] = emailTemplate;
+//       }
+
+//       else if(type == "SUPPRESSIONLIST") {
+//         supressArr.push(pData);
+//         Obj[type] = supressArr;
+//       }
+//       else if(type == "IDLIST") {
+//         idList.push(pData);
+//         Obj[type] = idList;
+//       }
+//     });
+  
+//     let resourceObj = {};
+
+//     for(const key in data) {
+//       resourceObj[key] = Obj;
+//     }
+  
+
+//     // console.log(resourceObj);
+
+ 
+//     causeArr.map((value) => {
+      
+       
+//         newArr.push(value.action);
+
+        
+//     })
+//     console.log(newArr);
+ 
+// }
+
+
+// /**** @Task : remove tab key from tabs */
+let data = {  
+  "tabs": [
+  {
+    "tab": {
+      "id": 1,
+      "name": "Dashboard",
+      "icon": "icon-speedometer",
+      "parentTabId": 0,
+      "createdAt": "2020-01-15T00:00:00.000Z",
+      "updatedAt": "2020-01-15T00:00:00.000Z"
+    }
+  },
+  {
+    "tab": {
+      "id": 5,
+      "name": "Users",
+      "icon": "cui-people\r\n\r\n",
+      "parentTabId": 0,
+      "createdAt": "2020-01-15T00:00:00.000Z",
+      "updatedAt": "2020-01-15T00:00:00.000Z"
+    }
+  },
+  {
+    "tab": {
+      "id": 2,
+      "name": "Causes",
+      "icon": "fa fa-files-o",
+      "parentTabId": 0,
+      "createdAt": "2020-01-15T00:00:00.000Z",
+      "updatedAt": "2020-01-15T00:00:00.000Z"
+    }
+  },
+  {
+    "tab": {
+      "id": 6,
+      "name": "Finance",
+      "icon": "icon-calculator",
+      "parentTabId": 0,
+      "createdAt": "2020-01-15T00:00:00.000Z",
+      "updatedAt": "2020-01-15T00:00:00.000Z"
+    }
+  },
+  {
+    "tab": {
+      "id": 3,
+      "name": "Donors",
+      "icon": "fa fa-dollar",
+      "parentTabId": 0,
+      "createdAt": "2020-01-15T00:00:00.000Z",
+      "updatedAt": "2020-01-15T00:00:00.000Z"
+    }
+  },
+  {
+    "tab": {
+      "id": 7,
+      "name": "Roles",
+      "icon": "fa fa-user-circle-o",
+      "parentTabId": 0,
+      "createdAt": "2020-01-15T00:00:00.000Z",
+      "updatedAt": "2020-01-15T00:00:00.000Z"
+    }
+  },
+  {
+    "tab": {
+      "id": 4,
+      "name": "Reports",
+      "icon": "fa fa-line-chart",
+      "parentTabId": 0,
+      "createdAt": "2020-01-15T00:00:00.000Z",
+      "updatedAt": "2020-01-15T00:00:00.000Z"
+    }
+  },
+  {
+    "tab": {
+      "id": 8,
+      "name": "Organization",
+      "icon": "fa fa-address-card-o",
+      "parentTabId": 0,
+      "createdAt": "2020-01-15T00:00:00.000Z",
+      "updatedAt": "2020-01-15T00:00:00.000Z"
+    }
+  },
+  {
+    "tab": {
+      "id": 11,
+      "name": "Campaigns",
+      "icon": "fa fa-bullhorn",
+      "parentTabId": 10,
+      "createdAt": "2020-08-13T00:00:00.000Z",
+      "updatedAt": "2020-08-13T00:00:00.000Z"
+    }
+  },
+  {
+    "tab": {
+      "id": 13,
+      "name": "Manage Templates",
+      "icon": "fa fa-folder",
+      "parentTabId": 10,
+      "createdAt": "2020-08-13T00:00:00.000Z",
+      "updatedAt": "2020-08-13T00:00:00.000Z"
+    }
+  },
+  {
+    "tab": {
+      "id": 10,
+      "name": "Email Marketing",
+      "icon": "icon-envelope-letter",
+      "parentTabId": 0,
+      "createdAt": "2020-08-13T00:00:00.000Z",
+      "updatedAt": "2020-08-13T00:00:00.000Z"
+    }
+  },
+  {
+    "tab": {
+      "id": 14,
+      "name": "Supression List",
+      "icon": "cui-list",
+      "parentTabId": 10,
+      "createdAt": "2020-08-13T00:00:00.000Z",
+      "updatedAt": "2020-08-13T00:00:00.000Z"
+    }
+  },
+  {
+    "tab": {
+      "id": 15,
+      "name": "Block List",
+      "icon": "fa fa-ban",
+      "parentTabId": 10,
+      "createdAt": "2020-08-13T00:00:00.000Z",
+      "updatedAt": "2020-08-13T00:00:00.000Z"
+    }
+  }
+],
+};
+
+
+// /** @function : remove tab key */
+const getTabs = () => {
+
+  let tabs = data.tabs;
+  let Obj = {};
+  let tabArr = [];
+  
+  for(let i =0;i < tabs.length;i++) {
+
+    let value = tabs[i];
+
+
+    for(let key in value) {
+      
+      let tabValue = value[key];
+      
+      tabArr.push(tabValue);
+
+      Obj[key] = tabArr;
+
+    
+
+    }
+
+
+
+  
+}
+console.log(Obj);
+}
+
+// let contextData = {
+//   "contexts": [
+//     {
+//       "id": 7,
+//       "roleId": 4,
+//       "contextId": 2,
+//       "createdAt": "2020-02-10T15:05:09.000Z",
+//       "updatedAt": "2020-02-10T15:05:09.000Z",
+//       "context": {
+//         "id": 2,
+//         "name": "USD",
+//         "description": "",
+//         "icon": null,
+//         "hasDropDown": true,
+//         "hasAccessOn": "USD",
+//         "showCauseCreateBtn": false,
+//         "canAssignCauseAdmin": true,
+//         "displayName": "USD",
+//         "dropDownConfig": "[{\"value\": \"state\",\"isVisible\": false,\"isPrimary\":false},{\"value\": \"usd\",\"isVisible\": true,\"isPrimary\":true},{\"value\": \"organization\",\"isVisible\": true,\"isPrimary\":false},{\"value\": \"club\",\"isVisible\": false,\"isPrimary\":false}]\r\n",
+//         "createdAt": "2020-01-15T00:00:00.000Z",
+//         "updatedAt": "2020-01-15T00:00:00.000Z"
+//       }
+//     },
+//     {
+//       "id": 8,
+//       "roleId": 4,
+//       "contextId": 3,
+//       "createdAt": "2020-02-10T15:05:09.000Z",
+//       "updatedAt": "2020-02-10T15:05:09.000Z",
+//       "context": {
+//         "id": 3,
+//         "name": "Organization",
+//         "description": "",
+//         "icon": null,
+//         "hasDropDown": true,
+//         "hasAccessOn": "ORG",
+//         "showCauseCreateBtn": true,
+//         "canAssignCauseAdmin": true,
+//         "displayName": "Organization",
+//         "dropDownConfig": "[{\"value\": \"state\",\"isVisible\": false,\"isPrimary\":false},{\"value\": \"usd\",\"isVisible\": false,\"isPrimary\":false},{\"value\": \"organization\",\"isVisible\": true,\"isPrimary\":true},{\"value\": \"club\",\"isVisible\": false,\"isPrimary\":false}]\r\n",
+//         "createdAt": "2020-01-15T00:00:00.000Z",
+//         "updatedAt": "2020-01-15T00:00:00.000Z"
+//       }
+//     }
+//   ],
+// };
+
+
+// function getContext () {
+
+//   let arr = [];
+//   let Obj = {};
+
+//   let data = contextData.contexts;
+
+//   for(let i =0;i < data.length;i++) {
+    
+//     let object = data[i].context;
+    
+//     arr.push(object);
+//     Obj["context"] = arr;
+
+
+//   }
+
+//   console.log(Obj);
+
+// }
+// }
